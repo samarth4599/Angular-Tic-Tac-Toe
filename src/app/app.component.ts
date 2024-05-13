01661c988e6f1f56254113916f578ed8cb02a01d
Samarth Goel <60538137+samarth4599@users.noreply.github.com>
2024-05-13 18:44:25 +0530

    Angular tick tack toe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,BoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
